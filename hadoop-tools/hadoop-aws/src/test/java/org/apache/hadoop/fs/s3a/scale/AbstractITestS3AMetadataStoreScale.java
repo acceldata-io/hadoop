b7ee22735f007fe44b7fd8db43789f2b1606e731
Ben Roling <ben.roling@cerner.com>
2019-05-20 02:59:54 +0530

    HADOOP-16085. S3Guard: use object version or etags to protect against inconsistent read after replace/overwrite.
    Contributed by Ben Roling.
    
    S3Guard will now track the etag of uploaded files and, if an S3
    bucket is versioned, the object version.
    
    You can then control how to react to a mismatch between the data
    in the DynamoDB table and that in the store: warn, fail, or, when
    using versions, return the original value.
    
    This adds two new columns to the table: etag and version.
    This is transparent to older S3A clients -but when such clients
    add/update data to the S3Guard table, they will not add these values.
    As a result, the etag/version checks will not work with files uploaded by older clients.
    
    For a consistent experience, upgrade all clients to use the latest hadoop version.

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/scale/AbstractITestS3AMetadataStoreScale.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/scale/AbstractITestS3AMetadataStoreScale.java
@@ -180,7 +180,8 @@ public abstract class AbstractITestS3AMetadataStoreScale extends
           status.getOwner());
     } else {
       return new S3AFileStatus(status.getLen(), status.getModificationTime(),
-          status.getPath(), status.getBlockSize(), status.getOwner());
+          status.getPath(), status.getBlockSize(), status.getOwner(),
+          status.getETag(), status.getVersionId());
     }
   }
 
@@ -217,7 +218,8 @@ public abstract class AbstractITestS3AMetadataStoreScale extends
   }
 
   protected static S3AFileStatus makeFileStatus(Path path) throws IOException {
-    return new S3AFileStatus(SIZE, ACCESS_TIME, path, BLOCK_SIZE, OWNER);
+    return new S3AFileStatus(SIZE, ACCESS_TIME, path, BLOCK_SIZE, OWNER,
+        null, null);
   }
 
   protected static S3AFileStatus makeDirStatus(Path p) throws IOException {

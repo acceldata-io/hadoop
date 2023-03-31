55237fd598296c617da1824a73a5d2e22495ef87
shubhamsharma <shubham@acceldata.io>
2023-03-31 11:13:18 +0530

    YARN-9877 Intermittent TIME_OUT of LogAggregationReport

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
@@ -1068,8 +1068,10 @@ public class RMAppImpl implements RMApp, Recoverable {
       // otherwise, add it to ranNodes for further process
       app.ranNodes.add(nodeAddedEvent.getNodeId());
 
-      app.logAggregation.addReportIfNecessary(
-          nodeAddedEvent.getNodeId(), app.getApplicationId());
+      if (!nodeAddedEvent.getFromAcquiredState()) {
+        app.logAggregation.addReportIfNecessary(
+                nodeAddedEvent.getNodeId(), app.getApplicationId());
+      }
     }
   }
 

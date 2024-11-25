99a8fb12bf65cb07ea9297604a0f58006f2e5e09
Deepak Damri <deepak.damri@acceldata.io>
2024-11-25 16:18:33 +0530

    HDFS-14846: libhdfs tests are failing on trunk due to jni usage bugs

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/hdfs.c
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/hdfs.c
@@ -3503,7 +3503,6 @@ done:
     destroyLocalReference(env, jUserName);
     destroyLocalReference(env, jGroupName);
     destroyLocalReference(env, jPermission);
-    destroyLocalReference(env, jPath);
     return jthr;
 }
 

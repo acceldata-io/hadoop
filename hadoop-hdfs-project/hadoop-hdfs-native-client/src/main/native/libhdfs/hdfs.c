dfc36236bfb169cb1f1b53d437e6e03008acaff9
harshith gandhe <harshith@acceldata.io>
2025-03-05 12:34:24 +0530

    Revert "HDFS-14846: libhdfs tests are failing on trunk due to jni usage bugs"
    This reverts commit 2a6fd6cecef6b439b3f7811af95e5e66c8a47b06.

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/hdfs.c
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/hdfs.c
@@ -3597,6 +3597,7 @@ done:
     destroyLocalReference(env, jUserName);
     destroyLocalReference(env, jGroupName);
     destroyLocalReference(env, jPermission);
+    destroyLocalReference(env, jPath);
     return jthr;
 }
 

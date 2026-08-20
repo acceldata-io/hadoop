d71fb4311684063fc581dcf8a5ee0ce0faffb505
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2026-08-20 10:49:55 -0400

    HADOOP-15984. Update jersey from 1.19 to 2.x. (#7019)
    Co-authored-by: Akira Ajisaka <aajisaka@apache.org>
    Co-authored-by: Viraj Jasani <vjasani@apache.org>
    Co-authored-by: Ayush Saxena <ayushsaxena@apache.org>
    Co-authored-by: PJ Fanning <fanningpj@apache.org>
    Co-authored-by: Steve Loughran <stevel@apache.org>
    Co-authored-by: He Xiaoqiao <hexiaoqiao@apache.org>
    Reviewed-by: Steve Loughran <stevel@apache.org>
    Reviewed-by: He Xiaoqiao <hexiaoqiao@apache.org>
    Signed-off-by: Shilun Fan <slfan1989@apache.org>
    (cherry picked from commit 06d36f5a6a1a01d159bef75b22ca2845e8c90bad)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/dao/CapacitySchedulerInfo.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/dao/CapacitySchedulerInfo.java
@@ -78,7 +78,7 @@ public class CapacitySchedulerInfo extends SchedulerInfo {
   public CapacitySchedulerInfo(CSQueue parent, CapacityScheduler cs) {
     this.queueName = parent.getQueueName();
     this.queuePath = parent.getQueuePath();
-    this.usedCapacity = parent.getUsedCapacity() * 100;
+    this.usedCapacity = parent.getUsedCapacity() * 100.0F;
     this.capacity = parent.getCapacity() * 100;
     this.queueCapacityVectorInfo = new QueueCapacityVectorInfo(
             parent.getConfiguredCapacityVector(NO_LABEL));
